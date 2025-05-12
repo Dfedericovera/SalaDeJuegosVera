2e4259bd35284980f9687ecda73dc13cf9a32b41
Federico Vera <dfedericovera@gmail.com>
2025-05-12 19:30:01 -0300

    Agrego bootstrap y componentes quienSoy, Home, Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { LoginComponent } from './componentes/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Home', component: HomeComponent },
+    { path: 'QuienSoy', component: QuienSoyComponent },
+    { path: 'Login', component: LoginComponent },
+];
